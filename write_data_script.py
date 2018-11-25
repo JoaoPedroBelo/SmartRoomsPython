7ee794237a3699fce0ec35e359d81d05b684c8d3
diogo-felix-martins <diogo.felix.martins@gmail.com>
2018-11-25 16:00:38 +0000

    init2

--- write_data_script.py
+++ write_data_script.py
@@ -31,42 +31,42 @@ if __name__ == "__main__":
     server.bind(server_address)
 
     while True:  # making a loop
-        #print('Waiting for sensors.')
-        #data, client = server.recvfrom(max_size)
-        #server.sendto(b'Data recieved', client)
+        print('Waiting for sensors.')
+        data, client = server.recvfrom(max_size)
+        server.sendto(b'Data recieved', client)
 
-        #if data == b'stop':
-        #    print("Stopping server.")
-        #    break
+        if data == b'stop':
+           print("Stopping server.")
+           break
 
         # print('RECIEVED: ', data, '  at  ', datetime.now())
         connection = connect_database()
         cursor = connection.cursor()
 
-        cursor.execute('''SELECT * FROM TBL_Eventos;''')
-        cursor.commit()
-
-        for d in cursor.description:
-            print(d[0])
-
-        print('')
-
-        for row in cursor.fetchall():
-            for field in row:
-                print(field)
-            print('')
+        # cursor.execute('''SELECT * FROM TBL_Eventos;''')
+        # cursor.commit()
+        #
+        # for d in cursor.description:
+        #     print(d[0])
+        #
+        # print('')
+        #
+        # for row in cursor.fetchall():
+        #     for field in row:
+        #         print(field)
+        #     print('')
 
         # INSERIR EVENTO RECEBIDO
-        #
-        # event_type, id_room = (data.decode("UTF-8")).split(",")
-        # timestamp = datetime.now()
-        #
-        # add_event = "INSERT INTO TBL_Eventos (tipo, time, TBL_Salas_id)" \
-        #             " VALUES (%s, %s, %s)"
-        #
-        # data_event = (event_type, timestamp, id_room)
-        #
-        # cursor.execute(add_event, data_event)  # Insert new event
+
+        event_type, id_room = (data.decode("UTF-8")).split(",")
+        timestamp = datetime.now()
+
+        add_event = "INSERT INTO TBL_Eventos" \
+                    " VALUES (%s, %s, %s)"
+
+        data_event = [(event_type, timestamp, id_room)]
+
+        cursor.execute(add_event, data_event)  # Insert new event
 
         connection.commit()
         cursor.close()
