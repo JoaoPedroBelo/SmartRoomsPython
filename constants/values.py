7da73d5eed91bb1c34c9639a334e3151e259c5e4
Diogo Martins <diogo.felix.martins@gmail.com>
2018-12-03 10:56:01 +0000

    added values file

---
+++ constants/values.py
@@ -0,0 +1,3 @@
+connection_string = 'DRIVER={SQL Server Native Client 11.0};Server=smartroomsdbserver.database.windows.net;PORT=1433;DATABASE=smartrooms_db;UID=smartrooms@smartroomsdbserver;PWD=SDgrupo3_projecto;TDS_Version=7.2;'
+# raspberry:
+# connection_string = 'DRIVER={FreeTDS};Server=smartroomsdbserver.database.windows.net;PORT=1433;DATABASE=smartrooms_db;UID=smartrooms@smartroomsdbserver;PWD=SDgrupo3_projecto;TDS_Version=7.2;'
