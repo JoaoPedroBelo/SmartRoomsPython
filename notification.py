1790410147855287c26dce3a2662b01307c8cffb
Joao Belo <Joao.Pedro.V.B@outlook.com>
2018-12-17 13:17:38 +0000

    add unsubscribe message

--- notification.py
+++ notification.py
@@ -90,7 +90,9 @@ if __name__ == "__main__":
     for user in get_users_subscrive(cursor):
         for rooms in get_user_rooms_data(cursor, user[0]):
             email = rooms[0]
-            message += str(rooms[2]) + " empty seats in " + rooms[1] + '\n' + str(rooms[3]) + " ocupied seats in " + rooms[1] + '\n'
+            message += str(rooms[2]) + " empty seats in " + rooms[1] + '\n' + str(rooms[3]) + " ocupied seats in " \
+                       + rooms[1] + '\n'
+        message = '\n'+"To unsubscribe visit the website insert your email and uncheck all the boxes "
         send_email(email, message)
         message = "the information of the room you have chosen: " + '\n'  # limpa mensagem depois do ciclo
 
